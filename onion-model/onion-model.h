df7c89ba99388909e0dbbb812a38e543ba644293
Patrick Goley <patrick.goley@gmail.com>
2015-02-25 00:29:26 -0600

    Initial Commit

---
+++ onion-model/onion-model.h
@@ -0,0 +1,19 @@
+//
+//  onion-model.h
+//  onion-model
+//
+//  Created by Patrick Goley on 2/25/15.
+//  Copyright (c) 2015 Patrick Goley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for onion-model.
+FOUNDATION_EXPORT double onion_modelVersionNumber;
+
+//! Project version string for onion-model.
+FOUNDATION_EXPORT const unsigned char onion_modelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <onion_model/PublicHeader.h>
+
+
